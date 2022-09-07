d7fdd1d418bc12dd0d27e91c42c9e60a8d267592
Rocio-Moroni <89430084+Rocio-Moroni@users.noreply.github.com>
2022-09-07 16:19:05 -0300

    Add files via upload

--- src/components/CheckAvailability/CheckAvailability.js
+++ src/components/CheckAvailability/CheckAvailability.js
@@ -1,47 +1,58 @@
-import React from 'react'
+/* IMPORTS */
+
+// SCSS import:
 import './CheckAvailability.scss';
+import './CheckAvailabilityMediaQuery.scss';
+// React import:
+import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap';
 
-function CheckAvailability() {
+
+/* COMPONENTS */
+
+// Check Availability component:
+function CheckAvailability () {
+
     return (
-    <Container fluid className='HomeFormContainer'>
-        <Row className='HomeFormRow'>
-            <form className='HomeForm'>
-                <Col xl={2} lg={3} md={3} sm={3} className='HomeFormTags'>
-                <label className='customField'>
-                    <input id='FormName' type='text' name='' placeholder="&nbsp;" required=''/>
-                    <span className='HomeFormPlaceholder'>Complete Name</span>
-                </label>
-                </Col>
-                
-                <Col xl={2} lg={3} md={3} sm={3} className='HomeFormTags'>
-                <label className='customField'>
-                    <input id='FormAdults' type='number' name='' placeholder="&nbsp;" required=''/>
-                    <span className='HomeFormPlaceholder'>Amount of adults</span>
-                </label>
-                </Col>            
-                
-                <Col xl={2} lg={3} md={3} sm={3} className='HomeFormTags'>
-                <label className='customField'>
-                    <input id='FormChildren' type='number' name='' placeholder="&nbsp;" required=''/>
-                    <span className='HomeFormPlaceholder'>Amount of children</span>
-                </label>
-                </Col>            
-                
-                <Col xl={2} lg={3} md={3} sm={3} className='HomeFormTags'>
-                <label className='customField'>
-                    <input id='FormNights' type='number' name='' placeholder="&nbsp;" required=''/>
-                    <span className='HomeFormPlaceholder'>Amount of nights</span>
-                </label>
-                </Col>
-                
-                <Col xl={2} lg={3} md={3} sm={3} className='HomeFormTags'>
-                    <button className='HomeFormBtn' id='HomeFormBtnId'><a>Check Availability</a></button>
-                </Col>
-            </form>
-        </Row>
-    </Container>
-)
+        <Container fluid className='HomeFormContainer'>
+            <Row className='HomeFormRow'>
+                <form className='HomeForm'>
+                    <Col  className='HomeFormTags'>
+                        <label className='CustomField'>
+                            <input className='input' id='FormName' type='text' name='' placeholder="&nbsp;" required=''/>
+                            <span className='Placeholder'>Complete Name</span>
+                        </label>
+                    </Col>
+
+                    <Col  className='HomeFormTags'>
+                        <label className='CustomField'>
+                            <input className='input' id='FormAdults' type='number' name='' placeholder="&nbsp;" required=''/>
+                            <span className='Placeholder'>Amount of adults</span>
+                        </label>
+                    </Col>
+
+                    <Col  className='HomeFormTags'>
+                        <label className='CustomField'>
+                            <input className='input' id='FormChildren' type='number' name='' placeholder="&nbsp;" required=''/>
+                            <span className='Placeholder'>Amount of children</span>
+                        </label>
+                    </Col>
+
+                    <Col  className='HomeFormTags'>
+                        <label className='CustomField'>
+                            <input className='input' id='FormNights' type='number' name='' placeholder="&nbsp;" required=''/>
+                            <span className='Placeholder'>Amount of nights</span>
+                        </label>
+                    </Col>
+
+                    <Col  className='HomeFormTags'>
+                        <button className='HomeFormBtn' id='HomeFormBtnId'> Check Availability </button>
+                    </Col>
+                </form>
+
+            </Row>
+        </Container>
+    )
 }
 
-export default CheckAvailability
+export default CheckAvailability;
