63d56bf01c228c9dec195c34cd699fe23f338a4d
Rocio-Moroni <89430084+Rocio-Moroni@users.noreply.github.com>
2022-09-07 16:19:45 -0300

    Add files via upload

--- src/components/HeroImage/HeroImage.js
+++ src/components/HeroImage/HeroImage.js
@@ -1,21 +1,31 @@
+/* IMPORTS */
+
+// SCSS import:
 import './HeroImage.scss';
+import './HeroImageMediaQuery.scss'
+// Components import:
+import CheckAvailability from '../CheckAvailability/CheckAvailability';
+// React import:
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-import CheckAvailability from '../CheckAvailability/CheckAvailability';
 
+
+/* COMPONENTS */
+
+// Hero Image Component:
 const HeroImage = () => {
 
     return(
-        <Container className='HeroImageContainer' fluid>
+        <Container fluid className='HeroImageContainer'>
             <Row>
                 <Col className='HeroCol'>
-                    <h1 className='HeroImageh1'>
+                    <h1 className='HeroImageH1'>
                         <div className='Welcome'>Welcome to</div>
                         <div className='HeroMessage'>
                             <span className="word">Estancia Las Cañitas</span><span className='wordWrapper'></span>
                         </div>
                     </h1>
-                    <img src='https://res.cloudinary.com/dfprmjlir/image/upload/v1662321866/CANITAS%20RENTAL/home__picture_jlbdjq.jpg' className='HeroImg'></img>
+                    <img src='https://res.cloudinary.com/dfprmjlir/image/upload/v1662321866/CANITAS%20RENTAL/home__picture_jlbdjq.jpg' className='HeroImg' alt=''></img>
                 </Col>
                 <CheckAvailability />
             </Row>
@@ -23,4 +33,4 @@ const HeroImage = () => {
     )
 }
 
-export default HeroImage
+export default HeroImage;
